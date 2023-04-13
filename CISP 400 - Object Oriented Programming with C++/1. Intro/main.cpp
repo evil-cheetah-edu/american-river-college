432c8c5985de273db7dd5d5e66a8f756a7e384aa
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-04-13 16:54:09 -0700

    format(): header and indentation

--- CISP 400 - Object Oriented Programming with C++/1. Intro/main.cpp
+++ CISP 400 - Object Oriented Programming with C++/1. Intro/main.cpp
@@ -1,16 +1,20 @@
-/********************************
+/**
+ * Eugene Moshchyn
+ * CISP 400 - MW 5:30 pm
+ * LINUX Lab
+ * February 3, 2019
+**/
 
-Eugene Moshchyn
-CISP 400 - MW 5:30 pm
-LINUX Lab
-February 3, 2019
-
-********************************/
 
 #include <iostream>
+
+
 using namespace std;
+
+
 int main()
 {
 	cout << "Hello World" << endl;
+    
 	return 0;
 }
