375121d6b910c023225ea9bfa6f445150cb0cedb
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-04-13 15:22:21 -0700

    format(): indentations

--- CISP 430 - Data Structures/5. Infix To Postfix/main.cpp
+++ CISP 430 - Data Structures/5. Infix To Postfix/main.cpp
@@ -2,13 +2,16 @@
 #include <string>
 #include <fstream>
 
+
 using namespace std;
+
 
 struct node
 {
-   char item;
-   node *next;
+    char item;
+    node *next;
 };
+
 
 bool isOperand(char c);
 void readFile(string &inPut);
@@ -23,6 +26,7 @@ string evaluateExpression(string inPut);
 
 
 ifstream in("input.txt");
+
 
 int main()
 {
@@ -48,121 +52,121 @@ int main()
 
 bool isOperand(char c)
 {
-   ///[48, 57]  is 0..9
-   ///[65, 90]  is A..Z
-   ///[97, 122] is a..z
-   return ((48 <= c && c <= 57) || 
-           (65 <= c && c <= 90) ||
-           (97 <= c && c <= 122));
+    ///[48, 57]  is 0..9
+    ///[65, 90]  is A..Z
+    ///[97, 122] is a..z
+    return ((48 <= c && c <= 57) || 
+            (65 <= c && c <= 90) ||
+            (97 <= c && c <= 122));
 }
 
 void readFile(string &inPut)
 {
-   if (in)
-   {
-      getline(in, inPut);
-   }
+    if (in)
+    {
+        getline(in, inPut);
+    }
 }
 
 ///Stack functions
 void  pop(struct node *&top)
 {
-   struct node *temp;
-   if (top)
-   {
-      temp = top;
-      top  = top->next;
-      delete temp;
-   }
+    struct node *temp;
+    if (top)
+    {
+        temp = top;
+        top  = top->next;
+        delete temp;
+    }
 }
 
 void push(struct node *&top, char item)
 {
-   struct node *newNode = new node;
+    struct node *newNode = new node;
 
-   newNode->item = item;
-   newNode->next = top;
-   top = newNode;
+    newNode->item = item;
+    newNode->next = top;
+    top = newNode;
 }
 //////////////////
 
 
 int precedence(char c)
 {
-   switch(c)
-   {
-      case('+'):
-      case('-'):
-         return 1;
+    switch(c)
+    {
+        case('+'):
+        case('-'):
+            return 1;
 
-      case('*'):
-      case('/'):
-         return 2;
+        case('*'):
+        case('/'):
+            return 2;
 
-      ///Even the brackets
-      ///Brackets are treated in evaluateExpression()
-      default:
-         return 0;
-   }
+        ///Even the brackets
+        ///Brackets are treated in evaluateExpression()
+        default:
+            return 0;
+    }
 }
 
 ///Adds the stack values to the string
 void popStack(struct node *&top, string &resultant, char currOperator)
 {
-   while(top && precedence(currOperator) <= precedence(top->item))
-   {
-      if ( !(top->item == '(' || top->item == ')') )
-         resultant += top->item;
+    while(top && precedence(currOperator) <= precedence(top->item))
+    {
+        if ( !(top->item == '(' || top->item == ')') )
+            resultant += top->item;
 
-      pop(top);
-   }
+        pop(top);
+    }
 }
 
 
 ///Evaluates the expression
 string evaluateExpression(string inPut)
 {
-   struct node *top = NULL;
-   string resultant = "";
+    struct node *top = NULL;
+    string resultant = "";
 
-   for (unsigned i = 0; i < inPut.size(); ++i)
-   {
-      if ( isOperand(inPut[i]) )
-         resultant += inPut[i];
+    for (unsigned i = 0; i < inPut.size(); ++i)
+    {
+        if ( isOperand(inPut[i]) )
+            resultant += inPut[i];
 
-      else
-      {
-         ///Is stack Empty?
-         if (!top)
-            ///Then Push
-            push(top, inPut[i]);
+        else
+        {
+            ///Is stack Empty?
+            if (!top)
+                ///Then Push
+                push(top, inPut[i]);
 
-         ///Is it open Parenthesis?
-         else if (inPut[i] == '(')
-            ///Then Push
-            push(top, inPut[i]);
-         
-         ///Is it close Parenthesis?
-         else if (inPut[i] == ')')
-            ///Then Pop
-            popStack(top, resultant);
-         
-         ///Is Precedence of Curr operator is >= than top operator?
-         else if ( precedence(inPut[i]) > precedence(top->item) )
-            ///Then Push
-            push(top, inPut[i]);
-         
-         ///Is Precedence of Curr Operator is < than top operator?
-         else if ( precedence(inPut[i]) <= precedence(top->item) )
-         {
-            ///Pop the stack, put everyting in resultant
-            ///Push Curr to stack
-            popStack(top, resultant, inPut[i]);
-            push(top, inPut[i]);
-         }
+            ///Is it open Parenthesis?
+            else if (inPut[i] == '(')
+                ///Then Push
+                push(top, inPut[i]);
+            
+            ///Is it close Parenthesis?
+            else if (inPut[i] == ')')
+                ///Then Pop
+                popStack(top, resultant);
+            
+            ///Is Precedence of Curr operator is >= than top operator?
+            else if ( precedence(inPut[i]) > precedence(top->item) )
+                ///Then Push
+                push(top, inPut[i]);
+            
+            ///Is Precedence of Curr Operator is < than top operator?
+            else if ( precedence(inPut[i]) <= precedence(top->item) )
+            {
+                ///Pop the stack, put everyting in resultant
+                ///Push Curr to stack
+                popStack(top, resultant, inPut[i]);
+                push(top, inPut[i]);
+            }
 
-       }
-    }
+        }
+        }
     ///Pop any other operators that are left in the stack
     popStack(top, resultant);
 
