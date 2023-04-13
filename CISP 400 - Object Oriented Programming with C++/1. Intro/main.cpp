6384835c61d5b3c3cfbc3c59f74f0c8e9368703a
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-04-13 16:53:11 -0700

    recover(): from coursework

---
+++ CISP 400 - Object Oriented Programming with C++/1. Intro/main.cpp
@@ -0,0 +1,16 @@
+/********************************
+
+Eugene Moshchyn
+CISP 400 - MW 5:30 pm
+LINUX Lab
+February 3, 2019
+
+********************************/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello World" << endl;
+	return 0;
+}
