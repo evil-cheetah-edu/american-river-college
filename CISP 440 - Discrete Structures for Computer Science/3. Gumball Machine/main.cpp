becfc9fd392f6b3a4197d8879ee682128d453f3c
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-04-12 20:50:05 -0700

    add(): comments for sectioning

--- CISP 440 - Discrete Structures for Computer Science/3. Gumball Machine/main.cpp
+++ CISP 440 - Discrete Structures for Computer Science/3. Gumball Machine/main.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 
 
+/**
+ * Enum for Machine State
+ */
 enum Q
 {
     q0,
@@ -13,6 +16,21 @@ enum Q
     q20
 };
 
+/**
+ * Set of strings corresponding to Machine State
+ */
+const string Q_STRINGS[] = {
+    "q0",
+    "q5",
+    "q10",
+    "q15",
+    "q20"
+};
+
+
+/**
+ * Enum for Machine Action
+ */
 enum O
 {
     GUMBALL,
@@ -22,23 +40,9 @@ enum O
     INVALID
 };
 
-enum I
-{
-    NICKEL,
-    DIME,
-    BUY,
-    QUIT
-};
-
-
-const string Q_STRINGS[] = {
-    "q0",
-    "q5",
-    "q10",
-    "q15",
-    "q20"
-};
-
+/**
+ * Set of strings corresponding to Machine Action 
+ */
 const string O_STRINGS[] = {
     "Gumball dispensed.  Have a nice day.\n",
     "Exact change required.  Returning coin.\n",
@@ -47,6 +51,21 @@ const string O_STRINGS[] = {
     "Invalid input.\n"
 };
 
+
+/**
+ * Enum for Coins 
+ */
+enum I
+{
+    NICKEL,
+    DIME,
+    BUY,
+    QUIT
+};
+
+/**
+ * Set of strings corresponding to Coins 
+ */
 const string I_STRINGS[] = {
     "NICKEL",
     "DIME",
@@ -55,6 +74,7 @@ const string I_STRINGS[] = {
 };
 
 
+/// Initial Prompt message to a User
 const string PROMPT_STRING = "NICKEL, DIME, BUY, or QUIT: ";
 
 
@@ -70,57 +90,67 @@ int main()
 
     while(input != I_STRINGS[QUIT])
     {
+        if (
+            input != I_STRINGS[BUY]    ||
+            input != I_STRINGS[NICKEL] ||
+            input != I_STRINGS[DIME]   ||
+            input != I_STRINGS[QUIT]
+        )
+            o = INVALID;
 
-        ///assign output and q based on current state and input
 
-        if (input != I_STRINGS[BUY] || input != I_STRINGS[NICKEL] || input != I_STRINGS[DIME] || input != I_STRINGS[QUIT] )
-             o = INVALID;
+        /// Check if State is Q0
+        if      (q == q0 && input == I_STRINGS[NICKEL])
+        {   q = q5;  o = NONE;  }
 
-
-        if (q == q0 && input == I_STRINGS[NICKEL])
-            {q = q5;
-             o = NONE;}
         else if (q == q0 && input == I_STRINGS[DIME])
-            {q = q10;
-             o = NONE;}
+        {   q = q10; o = NONE;  }
+
         else if (q == q0 && input == I_STRINGS[BUY])
-             o = MESSAGE;
+            o = MESSAGE;
 
 
+        /// Check if State is Q5
         else if (q == q5 && input == I_STRINGS[NICKEL])
-            {q = q10;
-             o = NONE;}
+        {   q = q10; o = NONE;  }
+
         else if (q == q5 && input == I_STRINGS[BUY])
             o = MESSAGE;
+
         else if (q == q5 && input == I_STRINGS[DIME])
-            {q = q15;
-             o = NONE;}
+        {   q = q15; o = NONE;  }
 
 
+        /// Check if State is Q10
         else if (q == q10 && input == I_STRINGS[NICKEL])
-            {q = q15;
-             o = NONE;}
+        {   q = q15; o = NONE;  }
+
         else if (q == q10 && input == I_STRINGS[DIME])
-            {q = q20;
-             o = NONE;}
+        {   q = q20; o = NONE;  }
+
         else if (q == q10 && input == I_STRINGS[BUY])
             o = MESSAGE;
 
+
+        /// Check if State is Q15
         else if (q == q15 && input == I_STRINGS[NICKEL])
-            {q = q20;
-             o = NONE;}
+        {   q = q20; o = NONE;  }
+
         else if (q == q15 && input == I_STRINGS[DIME])
             o = RETURN;
+
         else if (q == q15 && input == I_STRINGS[BUY])
             o = MESSAGE;
 
+
+        /// Check if State is Q20
         else if (q == q20 && input == I_STRINGS[BUY])
-            {o = GUMBALL;
-             q = q0;}
+        {   q = q0;  o = GUMBALL;  }
         else if (q == q20 && ( input == I_STRINGS[NICKEL] || input == I_STRINGS[DIME]))
             o = RETURN;
 
 
+        /// Set message based on `o` Enum
         if (o == MESSAGE)
                 output = O_STRINGS[o];
         else if (o == RETURN)
