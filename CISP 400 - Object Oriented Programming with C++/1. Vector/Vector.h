2e17ed490f3d51404c5dae0e6e58b7dc88ff2729
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-04-13 04:15:01 -0700

    format(): naming and tabs

--- CISP 400 - Object Oriented Programming with C++/1. Vector/Vector.h
+++ CISP 400 - Object Oriented Programming with C++/1. Vector/Vector.h
@@ -12,46 +12,45 @@ using namespace std;
 template <typename T>
 class Vector
 {
-	public:
-		Vector(int Vindex = 0);
-		Vector (const T*, unsigned s, int Vindex = 0);
-		Vector (const Vector&);
-		~Vector ();
+    public:
+        Vector(int Vindex = 0);
+        Vector(const T*, unsigned s, int Vindex = 0);
+        Vector(const Vector&);
+        ~Vector();
 
-		Vector& operator= (const Vector&);
+        Vector& operator=(const Vector&);
 
-		template <typename OT>
-		friend ostream& operator<< (ostream&, const Vector<OT>&);
-		template <typename OT>
-		friend ostream& operator<< (ostream&, const Vector<OT>*);
+        template <typename OT>
+        friend ostream& operator<<(ostream&, const Vector<OT>&);
 
-		T& operator[] (int Vindex);
-		const T& operator[] (int Vindex) const;
+        template <typename OT>
+        friend ostream& operator<<(ostream&, const Vector<OT>*);
 
-		Vector operator() (int Vfirst, int Vlast) const;
+        T& operator[](int Vindex);
+        const T& operator[](int Vindex) const;
 
-		Vector& operator+= (const T&);
-		Vector& operator+= (const Vector&);
+        Vector operator()(int Vfirst, int Vlast) const;
 
-		unsigned Length() const{return _length;};
+        Vector& operator+=(const T&);
+        Vector& operator+=(const Vector&);
 
-		void Remove (const int Vindex);
-		void Remove (int Vfirst, int Vlast);
-		void Remove ();
+        unsigned Length() const{return _length;};
 
-		void Insert (const T&, int Vindex);
-		void Insert (const T&);
+        void Remove(const int Vindex);
+        void Remove(int Vfirst, int Vlast);
+        void Remove();
+
+        void Insert(const T&, int Vindex);
+        void Insert(const T&);
 
         void write(ofstream&) const;
         void  read(ifstream&);
 
-  private:
+    private:
         T*       _a;
         unsigned _size;
         unsigned _length;
         int      _zero;
-
-
 };
 
 
