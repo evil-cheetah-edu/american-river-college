94c22c284fbb074e39f58cb0cdd7e744b2bfa38f
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-04-17 04:12:04 -0700

    format(): indentation

--- CISP 400 - Object Oriented Programming with C++/4. Marina/CabinCruiser.h
+++ CISP 400 - Object Oriented Programming with C++/4. Marina/CabinCruiser.h
@@ -7,31 +7,34 @@
 
 
 #include <iostream>
-using namespace std;
 #include "MotorPowered.h"
+
+
+using namespace std;
+
 
 #ifndef CABIN_CRUISER_H_
 #define CABIN_CRUISER_H_
 
+
 class CabinCruiser : public MotorPowered
 {
-  public:
-    CabinCruiser();
-    CabinCruiser(double disp, double len, double beam, const char* name, bool fb, FUEL_TYPE = DIESEL, MOTOR_DRIVE_TYPE = INBOARD, HULL_TYPE = SEMI_DISP);
-    CabinCruiser(const char* name);
-    ~CabinCruiser();
+    public:
+        CabinCruiser();
+        CabinCruiser(double disp, double len, double beam, const char* name, bool fb, FUEL_TYPE = DIESEL, MOTOR_DRIVE_TYPE = INBOARD, HULL_TYPE = SEMI_DISP);
+        CabinCruiser(const char* name);
+        ~CabinCruiser();
 
-    void Set_Flying_Bridge( bool );
-    bool Get_Flying_Bridge() const;
+        void Set_Flying_Bridge( bool );
+        bool Get_Flying_Bridge() const;
 
-    virtual void Propulsion_Maintenance();
-    virtual void Emergency_Procedures();
-    virtual void Display() const;
+        virtual void Propulsion_Maintenance();
+        virtual void Emergency_Procedures();
+        virtual void Display() const;
 
-  private:
-    bool _flying_bridge;
-
+    private:
+        bool _flying_bridge;
 };
 
-#endif
 
+#endif
