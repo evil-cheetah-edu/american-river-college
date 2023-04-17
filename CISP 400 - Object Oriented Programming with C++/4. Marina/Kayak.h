4a598acc9686f73c02489d96d975ad917f243fbd
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-04-17 04:19:15 -0700

    format(): indentations

--- CISP 400 - Object Oriented Programming with C++/4. Marina/Kayak.h
+++ CISP 400 - Object Oriented Programming with C++/4. Marina/Kayak.h
@@ -7,33 +7,36 @@
 
 
 #include <iostream>
-using namespace std;
 #include "MusclePowered.h"
+
+
+using namespace std;
+
 
 #ifndef KAYAK_H_
 #define KAYAK_H_
 
+
 class Kayak : public MusclePowered
 {
   public:
-    Kayak();
-    Kayak(double disp, double len, double beam, const char* name, bool ww, unsigned numSeats = 1, MUSCLE_DRIVE_TYPE = PADDLE, HULL_TYPE = DISPLACEMENT);
-    Kayak(const char* name);
-    virtual ~Kayak();
+        Kayak();
+        Kayak(double disp, double len, double beam, const char* name, bool ww, unsigned numSeats = 1, MUSCLE_DRIVE_TYPE = PADDLE, HULL_TYPE = DISPLACEMENT);
+        Kayak(const char* name);
+        virtual ~Kayak();
 
-    void Set_White_Water( bool );
-    bool Get_White_Water() const;
+        void Set_White_Water( bool );
+        bool Get_White_Water() const;
 
-    virtual void Propulsion_Maintenance();
-    virtual void Emergency_Procedures();
-    virtual void Display() const;
-    
-  private:
-    void     _verify_num_seats();
-    unsigned _num_seats;
-    bool     _white_water;
-
+        virtual void Propulsion_Maintenance();
+        virtual void Emergency_Procedures();
+        virtual void Display() const;
+        
+    private:
+        void     _verify_num_seats();
+        unsigned _num_seats;
+        bool     _white_water;
 };
 
-#endif
 
+#endif
