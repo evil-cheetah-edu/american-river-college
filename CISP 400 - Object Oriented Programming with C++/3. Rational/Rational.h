817ab77e02513ef6627504820e6df97f869884af
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-04-17 04:20:02 -0700

    format(): indentations

--- CISP 400 - Object Oriented Programming with C++/3. Rational/Rational.h
+++ CISP 400 - Object Oriented Programming with C++/3. Rational/Rational.h
@@ -5,88 +5,87 @@
  * March 13, 2019
 **/
 
-
-#ifndef _RATIONAL_H_
-#define _RATIONAL_H_
-
-
 #include <iostream>
 
 
 using namespace std;
 
 
+#ifndef _RATIONAL_H_
+#define _RATIONAL_H_
+
+
 class Rational
 {
-   public:
-      Rational();
-      Rational(long long P, long long Q = 1);
-      Rational(const Rational&);
+    public:
+        Rational();
+        Rational(long long P, long long Q = 1);
+        Rational(const Rational&);
 
-      Rational& operator=  (const Rational&);
-      Rational& operator+= (const Rational&);
-      Rational& operator-= (const Rational&);
-      Rational& operator*= (const Rational&);
-      Rational& operator/= (const Rational&);
+        Rational& operator=  (const Rational&);
+        Rational& operator+= (const Rational&);
+        Rational& operator-= (const Rational&);
+        Rational& operator*= (const Rational&);
+        Rational& operator/= (const Rational&);
 
-      friend ostream& operator<< (ostream&, const Rational&);
-      friend istream& operator>> (istream&, Rational&);
+        friend ostream& operator<< (ostream&, const Rational&);
+        friend istream& operator>> (istream&, Rational&);
 
-      Rational operator+ (const Rational&) const;
-      Rational operator+ (long long) const;
-      friend Rational operator+ (long long, const Rational&);
+        Rational operator+ (const Rational&) const;
+        Rational operator+ (long long) const;
+        friend Rational operator+ (long long, const Rational&);
 
-      Rational operator- (const Rational&) const;
-      Rational operator- (long long) const;
-      friend Rational operator- (long long, const Rational&);
+        Rational operator- (const Rational&) const;
+        Rational operator- (long long) const;
+        friend Rational operator- (long long, const Rational&);
 
-      Rational operator* (const Rational&) const;
-      Rational operator* (long long) const;
-      friend Rational operator* (long long, const Rational&);
+        Rational operator* (const Rational&) const;
+        Rational operator* (long long) const;
+        friend Rational operator* (long long, const Rational&);
 
-      Rational operator/ (const Rational&) const;
-      Rational operator/ (long long) const;
-      friend Rational operator/ (long long, const Rational&);
+        Rational operator/ (const Rational&) const;
+        Rational operator/ (long long) const;
+        friend Rational operator/ (long long, const Rational&);
 
-      bool operator== (const Rational&) const;
-      bool operator== (long long) const;
-      friend bool operator== (long long, const Rational&);
+        bool operator== (const Rational&) const;
+        bool operator== (long long) const;
+        friend bool operator== (long long, const Rational&);
 
-      bool operator!= (const Rational&) const;
-      bool operator!= (long long) const;
-      friend bool operator!= (long long, const Rational&);
+        bool operator!= (const Rational&) const;
+        bool operator!= (long long) const;
+        friend bool operator!= (long long, const Rational&);
 
-      bool operator> (const Rational&) const;
-      bool operator> (long long) const;
-      friend bool operator> (long long, const Rational&);
+        bool operator> (const Rational&) const;
+        bool operator> (long long) const;
+        friend bool operator> (long long, const Rational&);
 
-      bool operator< (const Rational&) const;
-      bool operator< (long long) const;
-      friend bool operator< (long long, const Rational&);
+        bool operator< (const Rational&) const;
+        bool operator< (long long) const;
+        friend bool operator< (long long, const Rational&);
 
-      bool operator>= (const Rational&) const;
-      bool operator>= (long long) const;
-      friend bool operator>= (long long, const Rational&);
+        bool operator>= (const Rational&) const;
+        bool operator>= (long long) const;
+        friend bool operator>= (long long, const Rational&);
 
-      bool operator<= (const Rational&) const;
-      bool operator<= (long long) const;
-      friend bool operator<= (long long, const Rational&);
+        bool operator<= (const Rational&) const;
+        bool operator<= (long long) const;
+        friend bool operator<= (long long, const Rational&);
 
-      Rational  operator++ (int); //post
-      Rational  operator-- (int); //post
-      Rational& operator++ ();    //pre
-      Rational& operator-- ();    //pre
-      Rational  operator-  () const;
-      Rational  operator+  () const;
+        Rational  operator++ (int); //post
+        Rational  operator-- (int); //post
+        Rational& operator++ ();    //pre
+        Rational& operator-- ();    //pre
+        Rational  operator-  () const;
+        Rational  operator+  () const;
 
-      Rational pow(unsigned exp) const; //return (*this)^exp
-      Rational inverse() const;         //return (*this)^-1
+        Rational pow(unsigned exp) const; //return (*this)^exp
+        Rational inverse() const;         //return (*this)^-1
 
 
-   private:
-      void _verify();
-      long long _p;
-      long long _q;
+    private:
+        void _verify();
+        long long _p;
+        long long _q;
 };
 
 
